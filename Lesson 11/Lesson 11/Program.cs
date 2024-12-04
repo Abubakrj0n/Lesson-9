4501866ac0764803fddfd93173365cc82e907a4e
Abubakrj0n <big500530@gmail.com>
2024-12-04 17:53:52 +0500

    Add files via upload

---
+++ Lesson 11/Lesson 11/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Program
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n<=0)
+            {
+                Console.WriteLine("error");
+                return;
+            }
+
+            int i = 1;
+            while (n>=1)
+            {
+                i *= n;
+                n -= 2;
+            }
+            Console.WriteLine($"Двойной фактериял равно {i}");
+        }
+    }
+}
+
